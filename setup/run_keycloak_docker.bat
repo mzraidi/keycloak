fac086f78cbfebaa8740d583c3b878b1e86b030b
mzraidi <96805002+mzraidi@users.noreply.github.com>
2022-12-05 11:40:36 +0100

    Update run_keycloak_docker.bat

--- setup/run_keycloak_docker.bat
+++ setup/run_keycloak_docker.bat
@@ -3,6 +3,6 @@
 setlocal
 
 rem replace this config with your own directory
-set WORKSHOP_HOME=C:\secure-oauth2-oidc-workshop\setup
+set WORKSHOP_HOME=C:\Users\mzrai\Downloads\keycloak-19.0.3
 
 docker run --rm -p 8080:8080 -e DB_VENDOR=h2 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin -e KEYCLOAK_IMPORT=/tmp/keycloak_realm_workshop.json -v %WORKSHOP_HOME%\keycloak_realm_workshop.json:/tmp/keycloak_realm_workshop.json jboss/keycloak:12.0.4
